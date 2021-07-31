f4c4b5f970153363d45994f05a500ea8b1979de8
ediprako <ediprakoso@gmail.com>
2021-07-31 11:07:51 +0700

    log to file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 func main() {
+	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("error when open file ", err)
+	}
+
+	log.SetOutput(file)
 	log.Println("Hello World!")
 }
